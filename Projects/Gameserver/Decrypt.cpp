4c047d83ec362f0ac2602659bc9a59b836a76c24
Djunio Filho <djunio_rmf@hotmail.com>
2023-03-06 21:40:30 -0300

    Revert "Update Decrypt.cpp"
    This reverts commit 9b7580677b36a704870de25f2ce124287b8cdf9c.

--- Projects/Gameserver/Decrypt.cpp
+++ Projects/Gameserver/Decrypt.cpp
@@ -4,36 +4,30 @@
 bool PacketDecrypt(BYTE* PacketBuffer)
 {
 	PacketHeader* Header = (PacketHeader*)PacketBuffer;
+
 	UINT32 KeyResult = NULL;
 	UINT32 KeyIncrement = KeyTable[(Header->Key & 255) * 2];
 
-	for (INT32 i = 4; i < Header->Size; i++, KeyIncrement++)
+	for (UINT32 i = 4; i < Header->Size; i++, KeyIncrement++)
 	{
-		INT32 keyResult = KeyTable[(((KeyIncrement & 255) & 0x800000FF) * 2) + 1];
-
+		KeyResult = KeyTable[((KeyIncrement & 0x800000FF) * 2) + 1];
 		switch (i & 3)
 		{
 		case 00:
-		{
-			PacketBuffer[i] = PacketBuffer[i] + ((keyResult & 255) << 1);
+			*(UINT8*)((UINT32)PacketBuffer + i) -= KeyResult << 1;
 			break;
-		}
 		case 01:
-		{
-			PacketBuffer[i] = PacketBuffer[i] - ((keyResult & 255) >> 3);
+			*(UINT8*)((UINT32)PacketBuffer + i) += KeyResult >> 3;
 			break;
-		}
 		case 02:
-		{
-			PacketBuffer[i] = PacketBuffer[i] + ((keyResult & 255) << 2);
+			*(UINT8*)((UINT32)PacketBuffer + i) -= KeyResult << 2;
 			break;
-		}
 		case 03:
-		{
-			PacketBuffer[i] = PacketBuffer[i] - ((keyResult & 255) >> 5);
+			*(UINT8*)((UINT32)PacketBuffer + i) += KeyResult >> 5;
 			break;
-		}
 		}
 	}
+
+	// TODO :Falta fazer a checagem do checksum
 	return true;
 }
